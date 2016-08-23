00fbdc7d3007040932be115fc050a12260489c3d
Edwin Rozario <edwin@boost.co.nz>
2016-08-23 14:59:57 +1200

    Replaced all the module descriptions to fix rubocop error

--- lib/supplejack/request.rb
+++ lib/supplejack/request.rb
@@ -1,9 +1,11 @@
-# The Supplejack Client code is Crown copyright (C) 2014, New Zealand Government,
+# The Supplejack Common code is
+# Crown copyright (C) 2014, New Zealand Government,
 # and is licensed under the GNU General Public License, version 3.
-# See https://github.com/DigitalNZ/supplejack_client for details.
+# See https://github.com/DigitalNZ/supplejack for details.
 #
-# Supplejack was created by DigitalNZ at the National Library of NZ
-# and the Department of Internal Affairs. http://digitalnz.org/supplejack
+# Supplejack was created by DigitalNZ at the
+# National Library of NZ and the Department of Internal Affairs.
+# http://digitalnz.org/supplejack
 
 require 'rest-client'
 
