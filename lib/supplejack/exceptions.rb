c3737f5190243581ca8d3959fb16e632c9a7e851
Edwin Rozario <edwin@boost.co.nz>
2016-08-24 16:45:45 +1200

    Fixed copyright description

--- lib/supplejack/exceptions.rb
+++ lib/supplejack/exceptions.rb
@@ -1,7 +1,7 @@
 # The Supplejack Common code is
 # Crown copyright (C) 2014, New Zealand Government,
 # and is licensed under the GNU General Public License, version 3.
-# See https://github.com/DigitalNZ/supplejack for details.
+# See https://github.com/DigitalNZ/supplejack_client for details.
 #
 # Supplejack was created by DigitalNZ at the
 # National Library of NZ and the Department of Internal Affairs.
