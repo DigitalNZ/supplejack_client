718fff08f35a596341bf4f07732a2933de072369
Edwin Rozario <edwin@boost.co.nz>
2017-05-01 14:30:29 +1200

    Why  : SJ client needs to send and receive subjects for user_sets What : - Added subjects to attributes        - Updated the specs

--- spec/supplejack/story_spec.rb
+++ spec/supplejack/story_spec.rb
@@ -138,7 +138,7 @@ module Supplejack
 
     describe '#save' do
       context 'Story is a new_record' do
-        let(:attributes) {{name: 'Story Name', description: nil, privacy: nil, copyright:nil, featured:nil, approved:nil, tags:nil, record_ids: nil}}
+        let(:attributes) {{name: 'Story Name', description: nil, privacy: nil, copyright:nil, featured:nil, approved:nil, tags:nil, subjects: nil, record_ids: nil}}
         let(:user) {{ api_key: 'foobar' }}
         let(:story) { Supplejack::Story.new(attributes.merge(user: user)) }
 
@@ -149,6 +149,7 @@ module Supplejack
               "name" => attributes[:name],
               "description" => "",
               "tags" => [],
+              "subjects" => [],
               "contents" => []
             }
           end
@@ -179,7 +180,7 @@ module Supplejack
       end
 
       context 'story is not new' do
-        let(:attributes) {{name: 'Story Name', description: 'desc', privacy: nil, copyright:nil, featured:nil, approved:nil, tags:nil, record_ids:nil}}
+        let(:attributes) {{name: 'Story Name', description: 'desc', privacy: nil, copyright:nil, featured:nil, approved:nil, tags:nil, subjects: nil, record_ids:nil}}
         let(:user) { { api_key: 'foobar' } }
         let(:story) { Supplejack::Story.new(attributes.merge(user: user, id: '123')) }
 
@@ -190,6 +191,7 @@ module Supplejack
               "name" => attributes[:name],
               "description" => "desc",
               "tags" => [],
+              "subjects" => [],
               "contents" => []
             }
           end
@@ -357,6 +359,7 @@ module Supplejack
           featured: nil, 
           approved: nil, 
           tags: nil,
+          subjects: nil,
           contents:  nil,
           created_at: nil,
           updated_at: nil, 
