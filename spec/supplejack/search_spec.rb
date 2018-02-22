14c7a4d388aa9dc2d3915310f767058f49393b01
Theo Paul <theo.markkus.paul@gmail.com>
2018-02-22 15:07:37 +1300

    Remove Supplejack License text from core files

--- spec/supplejack/search_spec.rb
+++ spec/supplejack/search_spec.rb
@@ -1,12 +1,3 @@
-# The Supplejack Common code is
-# Crown copyright (C) 2014, New Zealand Government,
-# and is licensed under the GNU General Public License, version 3.
-# See https://github.com/DigitalNZ/supplejack_client for details.
-#
-# Supplejack was created by DigitalNZ at the
-# National Library of NZ and the Department of Internal Affairs.
-# http://digitalnz.org/supplejack
-
 require 'spec_helper'
 
 class TestRecord
