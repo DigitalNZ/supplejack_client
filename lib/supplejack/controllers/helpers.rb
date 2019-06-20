00477efea54cfca3a3594b236942f4aa9ad8d7b3
Tim Wright <tim@boost.co.nz>
2019-06-20 18:04:28 +1200

    tidy up

--- lib/supplejack/controllers/helpers.rb
+++ lib/supplejack/controllers/helpers.rb
@@ -144,7 +144,7 @@ module Supplejack
         if record.previous_record
           options[:page] = record.previous_page if record.previous_page.to_i > 1
           path = record_path(record.previous_record, search: options)
-          path = "#{record_path(record.previous_record, search: options)}?#{request.query_string}" if html_options[:include_querystring]
+          path = "#{path}?#{request.query_string}" if html_options[:include_querystring]
           links += link_to(raw(previous_label), path, class: html_options[:prev_class]).html_safe
         else
           links += content_tag(:span, previous_label, class: html_options[:prev_class])
@@ -159,7 +159,7 @@ module Supplejack
         if record.next_record
           options[:page] = record.next_page if record.next_page.to_i > 1
           path = record_path(record.next_record, search: options)
-          path = "#{record_path(record.next_record, search: options)}?#{request.query_string}" if html_options[:include_querystring]
+          path = "#{path}?#{request.query_string}" if html_options[:include_querystring]
           links += link_to(raw(next_label), path, class: html_options[:next_class]).html_safe
         else
           links += content_tag(:span, next_label, class: html_options[:next_class])
