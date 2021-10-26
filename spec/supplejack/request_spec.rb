fec784f7ceff004d3e1329c306293080cc5848d4
Edwin Rozario <rozarioed@gmail.com>
2021-10-26 13:32:44 +1300

    refactor(Supplejack::Request): specs

--- spec/supplejack/request_spec.rb
+++ spec/supplejack/request_spec.rb
@@ -2,14 +2,17 @@
 
 require 'spec_helper'
 
-class Test
-  include Supplejack::Request
+module Supplejack
+  class TestClass
+    include Supplejack::Request
+  end
 end
 
 module Supplejack
   describe Request do
+    let(:subject) { Supplejack::TestClass.new }
+
     before(:each) do
-      @test = Test.new
       Supplejack.stub(:api_key) { '123' }
       Supplejack.stub(:api_url) { 'http://api.org' }
       Supplejack.stub(:timeout) { 20 }
@@ -20,12 +23,12 @@ module Supplejack
 
       it 'serializes the parameters in the url' do
         RestClient::Request.should_receive(:execute).with(url: "http://api.org/records.json?#{{ and: { name: 'John' } }.to_query}&api_key=123", method: :get, read_timeout: 20)
-        @test.get('/records', and: { name: 'John' })
+        subject.get('/records', and: { name: 'John' })
       end
 
       it 'parses the JSON returned' do
         RestClient::Request.stub(:execute).and_return(%( {"search": {}} ))
-        @test.get('/records').should eq('search' => {})
+        subject.get('/records').should eq('search' => {})
       end
 
       it 'logs a error correctly when the response from the API failed' do
@@ -33,32 +36,32 @@ module Supplejack
         @subscriber = Supplejack::LogSubscriber.new
         Supplejack::LogSubscriber.stub(:new) { @subscriber }
         @subscriber.should_receive(:log_request)
-        @test.get('/records')
+        subject.get('/records')
       end
 
       context 'request format' do
         it 'executes a request in a json format' do
           RestClient::Request.should_receive(:execute).with(hash_including(url: 'http://api.org/records.json?api_key=123'))
-          @test.get('/records')
+          subject.get('/records')
         end
 
         it 'overrides the response format with xml' do
           RestClient::Request.should_receive(:execute).with(hash_including(url: 'http://api.org/records.xml?api_key=123'))
-          @test.get('/records', {}, format: :xml)
+          subject.get('/records', {}, format: :xml)
         end
       end
 
       context 'api key' do
         it 'overrides the api key' do
           RestClient::Request.should_receive(:execute).with(hash_including(url: 'http://api.org/records.json?api_key=456'))
-          @test.get('/records', api_key: '456')
+          subject.get('/records', api_key: '456')
         end
       end
 
       context 'timeout' do
         it 'calculates the timeout' do
-          @test.should_receive(:timeout).with(hash_including(timeout: 60))
-          @test.get('/', {}, timeout: 60)
+          subject.should_receive(:timeout).with(hash_including(timeout: 60))
+          subject.get('/', {}, timeout: 60)
         end
       end
 
@@ -67,7 +70,7 @@ module Supplejack
           RestClient::Request.stub(:execute).and_raise(RestClient::ServiceUnavailable)
           RestClient::Request.should_receive(:execute).exactly(5).times
           expect do
-            @test.get('/records')
+            subject.get('/records')
           end.to raise_error(RestClient::ServiceUnavailable)
         end
       end
@@ -78,28 +81,28 @@ module Supplejack
 
       it 'executes a post request' do
         RestClient::Request.should_receive(:execute).with(hash_including(method: :post))
-        @test.post('/records/1/ucm', {})
+        subject.post('/records/1/ucm', {})
       end
 
       it 'passes the payload along' do
         payload = { 'ucm_record' => { name: 'geocords', value: '1234' } }
         RestClient::Request.should_receive(:execute).with(hash_including(payload: payload.to_json))
-        @test.post('/records/1/ucm', {}, payload)
+        subject.post('/records/1/ucm', {}, payload)
       end
 
       it 'adds the extra parameters to the post request' do
-        @test.should_receive(:full_url).with('/records/1/ucm', nil, api_key: '12344')
-        @test.post('/records/1/ucm', { api_key: '12344' }, {})
+        subject.should_receive(:full_url).with('/records/1/ucm', nil, api_key: '12344')
+        subject.post('/records/1/ucm', { api_key: '12344' }, {})
       end
 
       it 'adds json headers and converts the payload into json' do
         RestClient::Request.should_receive(:execute).with(hash_including(headers: { content_type: :json, accept: :json }, payload: { records: [{ record_id: 1, position: 1 }, { record_id: 2, position: 2 }] }.to_json))
-        @test.post('/records/1/ucm', {}, records: [{ record_id: 1, position: 1 }, { record_id: 2, position: 2 }])
+        subject.post('/records/1/ucm', {}, records: [{ record_id: 1, position: 1 }, { record_id: 2, position: 2 }])
       end
 
       it 'parses the JSON response' do
         RestClient::Request.stub(:execute) { { user: { name: 'John' } }.to_json }
-        @test.post('/users', {}, {}).should eq('user' => { 'name' => 'John' })
+        subject.post('/users', {}, {}).should eq('user' => { 'name' => 'John' })
       end
     end
 
@@ -108,12 +111,12 @@ module Supplejack
 
       it 'executes a delete request' do
         RestClient::Request.should_receive(:execute).with(hash_including(method: :delete))
-        @test.delete('/records/1/ucm/1')
+        subject.delete('/records/1/ucm/1')
       end
 
       it 'adds the extra parameters to the delete request' do
-        @test.should_receive(:full_url).with('/records/1/ucm/1', nil, api_key: '12344')
-        @test.delete('/records/1/ucm/1', api_key: '12344')
+        subject.should_receive(:full_url).with('/records/1/ucm/1', nil, api_key: '12344')
+        subject.delete('/records/1/ucm/1', api_key: '12344')
       end
     end
 
@@ -122,27 +125,27 @@ module Supplejack
 
       it 'executes a put request' do
         RestClient::Request.should_receive(:execute).with(hash_including(method: :put))
-        @test.put('/records/1/ucm/1')
+        subject.put('/records/1/ucm/1')
       end
 
       it 'passes the payload along' do
         RestClient::Request.should_receive(:execute).with(hash_including(payload: { name: 1 }.to_json))
-        @test.put('/records/1/ucm/1', {}, name: 1)
+        subject.put('/records/1/ucm/1', {}, name: 1)
       end
 
       it 'adds the extra parameters to the put request' do
-        @test.should_receive(:full_url).with('/records/1/ucm/1', nil, api_key: '12344')
-        @test.put('/records/1/ucm/1', { api_key: '12344' }, {})
+        subject.should_receive(:full_url).with('/records/1/ucm/1', nil, api_key: '12344')
+        subject.put('/records/1/ucm/1', { api_key: '12344' }, {})
       end
 
       it 'adds json headers and converts the payload into json' do
         RestClient::Request.should_receive(:execute).with(hash_including(headers: { content_type: :json, accept: :json }, payload: { records: [1, 2, 3] }.to_json))
-        @test.put('/records/1/ucm/1', {}, records: [1, 2, 3])
+        subject.put('/records/1/ucm/1', {}, records: [1, 2, 3])
       end
 
       it 'parses the JSON response' do
         RestClient::Request.stub(:execute) { { user: { name: 'John' } }.to_json }
-        @test.put('/users/1', {}, {}).should eq('user' => { 'name' => 'John' })
+        subject.put('/users/1', {}, {}).should eq('user' => { 'name' => 'John' })
       end
     end
 
@@ -152,74 +155,74 @@ module Supplejack
       it 'executes a patch request' do
         RestClient::Request.should_receive(:execute).with(hash_including(method: :patch))
 
-        @test.patch('/records/1/ucm/1')
+        subject.patch('/records/1/ucm/1')
       end
 
       it 'passes the payload along' do
         RestClient::Request.should_receive(:execute).with(hash_including(payload: { name: 1 }.to_json))
 
-        @test.put('/records/1/ucm/1', {}, name: 1)
+        subject.put('/records/1/ucm/1', {}, name: 1)
       end
 
       it 'adds the extra parameters to the patch request' do
-        @test.should_receive(:full_url).with('/records/1/ucm/1', nil, api_key: '12344')
+        subject.should_receive(:full_url).with('/records/1/ucm/1', nil, api_key: '12344')
 
-        @test.patch('/records/1/ucm/1', { api_key: '12344' }, {})
+        subject.patch('/records/1/ucm/1', { api_key: '12344' }, {})
       end
 
       it 'adds json headers and converts the payload into json' do
         RestClient::Request.should_receive(:execute).with(hash_including(headers: { content_type: :json, accept: :json }, payload: { records: [1, 2, 3] }.to_json))
 
-        @test.patch('/records/1/ucm/1', {}, records: [1, 2, 3])
+        subject.patch('/records/1/ucm/1', {}, records: [1, 2, 3])
       end
 
       it 'parses the JSON response' do
         RestClient::Request.stub(:execute) { { user: { name: 'John' } }.to_json }
 
-        @test.patch('/users/1', {}, {}).should eq('user' => { 'name' => 'John' })
+        subject.patch('/users/1', {}, {}).should eq('user' => { 'name' => 'John' })
       end
     end
 
     describe '#timeout' do
       it 'defaults to the timeout in the configuration' do
-        expect(@test.send(:timeout)).to eq 20
+        expect(subject.send(:timeout)).to eq 20
       end
 
       it 'defaults to 30 when not set in the configuration' do
         Supplejack.stub(:timeout) { nil }
 
-        expect(@test.send(:timeout)).to eq 15
+        expect(subject.send(:timeout)).to eq 15
       end
 
       it 'overrides the timeout' do
-        expect(@test.send(:timeout, timeout: 60)).to eq 60
+        expect(subject.send(:timeout, timeout: 60)).to eq 60
       end
     end
 
     describe '#full_url' do
       it 'returns the full url with default api_url, format and api_key' do
-        @test.send(:full_url, '/records').should eq('http://api.org/records.json?api_key=123')
+        subject.send(:full_url, '/records').should eq('http://api.org/records.json?api_key=123')
       end
 
       it 'overrides the format' do
-        @test.send(:full_url, '/records', 'xml').should eq('http://api.org/records.xml?api_key=123')
+        subject.send(:full_url, '/records', 'xml').should eq('http://api.org/records.xml?api_key=123')
       end
 
       it 'overrides the api key' do
-        @test.send(:full_url, '/records', nil, api_key: '456').should eq('http://api.org/records.json?api_key=456')
+        subject.send(:full_url, '/records', nil, api_key: '456').should eq('http://api.org/records.json?api_key=456')
       end
 
       it 'url encodes the parameters' do
-        @test.send(:full_url, '/records', nil, api_key: '456', i: { category: 'Images' }).should eq('http://api.org/records.json?api_key=456&i%5Bcategory%5D=Images')
+        subject.send(:full_url, '/records', nil, api_key: '456', i: { category: 'Images' }).should eq('http://api.org/records.json?api_key=456&i%5Bcategory%5D=Images')
       end
 
       it 'adds debug=true when enable_debugging is set to true' do
         Supplejack.stub(:enable_debugging) { true }
-        @test.send(:full_url, '/records', nil, {}).should eq 'http://api.org/records.json?api_key=123&debug=true'
+        subject.send(:full_url, '/records', nil, {}).should eq 'http://api.org/records.json?api_key=123&debug=true'
       end
 
       it 'handles nil params' do
-        @test.send(:full_url, '/records', nil, nil).should eq 'http://api.org/records.json?api_key=123'
+        subject.send(:full_url, '/records', nil, nil).should eq 'http://api.org/records.json?api_key=123'
       end
     end
   end
