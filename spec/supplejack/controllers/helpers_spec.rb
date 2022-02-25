42ef6144243fa83a6b4063dea721af44a2825f61
Richard Matthews <richard@boost.co.nz>
2022-02-25 13:23:35 +1300

    Attempt to fix helpers_spec

--- spec/supplejack/controllers/helpers_spec.rb
+++ spec/supplejack/controllers/helpers_spec.rb
@@ -55,7 +55,7 @@ module Supplejack
         before { allow(controller).to receive(:params).and_return({ text: 'dog' }) }
 
         it 'initializes a search object with the params' do
-          expect(Supplejack::Search).to receive(:new).with(text: 'dog')
+          expect(Supplejack::Search).to receive(:new).with({text: 'dog'})
 
           controller.search
         end
@@ -63,20 +63,20 @@ module Supplejack
         it 'tries to initialize with params[:search] ' do
           allow(controller).to receive(:params).and_return({ search: { text: 'cat' } })
 
-          expect(Supplejack::Search).to receive(:new).with(text: 'cat')
+          expect(Supplejack::Search).to receive(:new).with({text: 'cat'})
 
           controller.search
         end
 
         it 'initializes the search with the passed params' do
-          expect(Supplejack::Search).to receive(:new).with(text: 'elephant')
+          expect(Supplejack::Search).to receive(:new).with({text: 'elephant'})
 
-          controller.search(text: 'elephant')
+          controller.search({text: 'elephant'})
         end
 
         it 'uses the special Search class' do
           allow(Supplejack).to receive(:search_klass).and_return('AdvancedSearch')
-          expect(AdvancedSearch).to receive(:new).with(text: 'dog')
+          expect(AdvancedSearch).to receive(:new).with({text: 'dog'})
 
           controller.search
         end
